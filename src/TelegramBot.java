f80b17d0f4a617c5898b9ac480d30e66bc4fc7ec
Danil Vildanov <danillykk@gmail.com>
2017-11-17 13:20:13 +0300

    add telegramBot.java

--- src/TelegramBot.java
+++ src/TelegramBot.java
@@ -1,4 +1,3 @@
-import com.sun.org.apache.regexp.internal.RE;
 import entity.*;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
@@ -23,7 +22,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private static DataInitializer dataInitializer = new DataInitializer();
     private static Cook cook;
 
-    private static void init(){
+    private static void init() {
         storageService = dataInitializer.initDataService();
         menuService = dataInitializer.initMenuService();
         dishService = dataInitializer.initDishService();
@@ -61,45 +60,41 @@ public class TelegramBot extends TelegramLongPollingBot {
                 sendMsg(message, "Привет, я робот");
                 sendMsg(message, "Чтобы увидеть меню напиши:");
                 sendMsg(message, "Сделать заказ");
-            }
-            else if (cook.getState().equals(CookStates.FREE) &&  message.getText().equals("Сделать заказ")) {
+            } else if (cook.getState().equals(CookStates.FREE) && message.getText().equals("Сделать заказ")) {
                 showMenu(message);
                 cook.setState(CookStates.HAS_ORDER);
-            }
-            else if (cook.getState().equals(CookStates.COOKING) && messageIsAction(message)) {
+            } else if (cook.getState().equals(CookStates.COOKING) && messageIsAction(message)) {
                 //то применяется экшн к повару
                 //чек реактивных реакций
                 //Эмоции повара применяются к блюду
-            }
-            else if (cook.getState().equals(CookStates.HAS_ORDER) && messageIsADish(message)) {
+            } else if (cook.getState().equals(CookStates.HAS_ORDER) && messageIsADish(message)) {
 //                sendMsg(message, "Начинаю готовить");
                 cook.setState(CookStates.COOKING);
                 startCooking(message);
-            }
-            else
+            } else
                 sendMsg(message, "Я не знаю что ответить на это");
         }
     }
 
     private void startCooking(Message message) {
 
-            if (recipeService.iteratorNext()) {
-                Step step = recipeService.getCurrentStepInRecipe();
-                showStepAndHisAction(message, step);
+        if (recipeService.iteratorNext()) {
+            Step step = recipeService.getCurrentStepInRecipe();
+            showStepAndHisAction(message, step);
 
-                //show steps by timer
-                TimerTask timerTask = new TimerTask() {
-                    @Override
-                    public void run() {
-                        startCooking(message);
-                    }
-                };
+            //show steps by timer
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    startCooking(message);
+                }
+            };
 
-                Timer timer = new Timer();
-                timer.schedule(timerTask, step.getTime() * 1000);
-            } else {
-                cook.setState(CookStates.FREE);
-            }
+            Timer timer = new Timer();
+            timer.schedule(timerTask, step.getTime() * 1000);
+        } else {
+            cook.setState(CookStates.FREE);
+        }
 
     }
 
@@ -107,7 +102,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private void showStepAndHisAction(Message message, Step step) {
         List<Action> actions = recipeService.getAllActionsInCurrentStep();
         List<String> actionButtons = new ArrayList<>();
-        for (Action action: actions) {
+        for (Action action : actions) {
             actionButtons.add(action.getName());
         }
 
@@ -122,7 +117,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
 
-        for (String button: buttonsName) {
+        for (String button : buttonsName) {
             KeyboardRow row = new KeyboardRow();
             row.add(button);
             keyboard.add(row);
@@ -142,7 +137,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private boolean messageIsADish(Message message) {
-        for (Recipe recipe: menuService.getMenu()) {
+        for (Recipe recipe : menuService.getMenu()) {
             if (message.getText().startsWith(recipe.getName())) {
                 recipeService.setRecipe(recipe.getId());
                 recipeService.initIterator();
@@ -153,7 +148,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private boolean messageIsAction(Message message) {
-        for (Action action: recipeService.getAllActionsInCurrentStep()) {
+        for (Action action : recipeService.getAllActionsInCurrentStep()) {
             if (message.getText().startsWith(action.getName())) {
                 applyActionOnCook(message, action);
                 return true;
@@ -169,12 +164,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void getCookInfo(Message message) {
-        sendMsg(message,cookService.getCook().toString());
+        sendMsg(message, cookService.getCook().toString());
     }
 
     private void showMenu(Message message) {
-        List<String> buttonsName = new  ArrayList<>();
-        for (Recipe recipe: menuService.getMenu()) {
+        List<String> buttonsName = new ArrayList<>();
+        for (Recipe recipe : menuService.getMenu()) {
             buttonsName.add(recipe.getName() + " " + recipe.getCookingTime() + "c");
         }
         showButtons(buttonsName, "Вот что мы можем вам предложить", message);
