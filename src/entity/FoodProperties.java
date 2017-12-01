970c26434e82d252038e706c359254b06d63c2da
SeregaKuznetsov <SeregaKuznetsov98@gmail.com>
2017-12-01 12:10:54 +0300

    add food properties

--- src/entity/FoodProperties.java
+++ src/entity/FoodProperties.java
@@ -2,13 +2,85 @@ package entity;
 
 public class FoodProperties {
 
-    private int quality;
 
-    public int getQuality() {
-        return quality;
+    //Внешний вид
+    private int view;
+    //Острота
+    private int keenness;
+    //Соленость
+    private int salinity;
+    //Сладость
+    private int sweetness;
+    //Кислотность
+    private int sourness;
+    //Горький
+    private int bitter;
+    //Степень прожарки
+    private int roasting;
+    //Запах
+    private int smell;
+
+    public int getView() {
+        return view;
     }
 
-    public void setQuality(int quality) {
-        this.quality = quality;
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    public int getKeenness() {
+        return keenness;
+    }
+
+    public void setKeenness(int keenness) {
+        this.keenness = keenness;
+    }
+
+    public int getSalinity() {
+        return salinity;
+    }
+
+    public void setSalinity(int salinity) {
+        this.salinity = salinity;
+    }
+
+    public int getSweetness() {
+        return sweetness;
+    }
+
+    public void setSweetness(int sweetness) {
+        this.sweetness = sweetness;
+    }
+
+    public int getSourness() {
+        return sourness;
+    }
+
+    public void setSourness(int sourness) {
+        this.sourness = sourness;
+    }
+
+    public int getBitter() {
+        return bitter;
+    }
+
+    public void setBitter(int bitter) {
+        this.bitter = bitter;
+    }
+
+    public int getRoasting() {
+        return roasting;
+    }
+
+    public void setRoasting(int roasting) {
+        this.roasting = roasting;
+    }
+
+    public int getSmell() {
+        return smell;
+    }
+
+    public void setSmell(int smell) {
+        this.smell = smell;
     }
 }
